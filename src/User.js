bfb8814af167934a85db6c882ccdc50c6ff845c8
Joshua Laurence <laurence11@fb.com>
2022-10-21 10:16:26 +0100

    Finished User Class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,20 @@
 class User {
   // User code here
+  #username
+  #password
+  #age
+
+  constructor(username, password, age) {
+    this.#username = username
+    this.#password = password
+    this.#age = age
+  }
+
+  //Getters
+  get username() {return this.#username}
+  get password() {return this.#password}
+  get age() {return this.#age}
+
 }
 
 module.exports = User
